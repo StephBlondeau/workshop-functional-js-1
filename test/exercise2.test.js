79b85544120e3da2646f793fe41c859872f7fe56
Blondeau <stephanie.blondeau@bts-malraux.net>
2017-04-21 10:12:06 +0200

    Exercice 3

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -6,8 +6,7 @@ describe('GIVEN function reverseYolo', function() {
   it('WHEN my mystery input THEN my wonderful output', function() {
 
     // Your future job begins here ...
-    let whereIsMyMind = '???';
-
+    let whereIsMyMind = ['TRONKG330', [0,0,1,1,2,2,4,7]];
     expect(reverseYolo(whereIsMyMind)).to.eql([ 'T', 'R', 'O', 'N', 'K', 'G', '3', '3', '0', 7, 4, 2, 2, 1, 1, 0, 0 ]);
   });
 
